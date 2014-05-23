1b1d856dae68ae3574d5926405ef78194bb06a29
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-05-23 08:17:48 -0600

    bettwe formating for transtion prob output

--- transition_probability.py
+++ transition_probability.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 from operator import itemgetter
 from readdata import read_data
 
+RN = ['I', 'bII', 'II', 'bIII', 'III', 'IV', 'bV', 'V', 'bVI', 'VI', 'bVII', 'VII']
+
 def get_overall_counts(chord_lists):
     """Return dictionaries with individual chord counts and transition counts."""
     chord_counts = defaultdict(lambda: 0)
@@ -35,5 +37,7 @@ if __name__ == '__main__':
     chord_counts, transition_counts = get_overall_counts(data)
     transition_probs = get_transition_probs(chord_counts, transition_counts)
 
-    for transition, prob in sorted(transition_probs.items(), key=itemgetter(1), reverse=True):
-        print transition, '\t', prob
+    # map roman numerals to integers for sorting, and covert back to display
+    transitions = [(RN.index(c1), c2) for c1, c2 in transition_probs]
+    for c1, c2 in sorted(transitions):
+        print '({} -> {}): {:.4f}'.format(RN[c1], c2, transition_probs[(RN[c1], c2)])
