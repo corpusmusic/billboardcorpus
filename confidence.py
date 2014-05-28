c5fa004e47b015e86d9d1dada4fc9893798b0ee8
Andrew Christopher Mahan <anma8252@musn1b49mac126.colorado.edu>
2014-05-28 11:06:16 -0600

    messing with confidence Intervals

---
+++ confidence.py
@@ -0,0 +1,15 @@
+import numpy as np
+import scipy as sp
+import scipy.stats
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0*np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
+    return m, m-h, m+h
+
+
+problist = [.4,.45,.3,.7,.3,.34,1] 
+
+mean_confidence_intercal(problist)
