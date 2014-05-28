dee387ba048c48ace27bd97f8275e96bdbe3f852
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-05-27 18:35:04 -0600

    transition probs by song

--- transition_probability2.py
+++ transition_probability2.py
@@ -6,24 +6,29 @@ from readdata import read_data
 
 RN = ['I', 'bII', 'II', 'bIII', 'III', 'IV', 'bV', 'V', 'bVI', 'VI', 'bVII', 'VII', 'NonHarmonic']
 
-def get_overall_counts(chord_lists):
+def transition_probs_by_song(chord_lists):
     """Return dictionaries with individual chord counts and transition counts."""
     chord_counts = defaultdict(lambda: 0)
     transition_counts = defaultdict(lambda: 0)
-    song_counts = defaultdict(lambda: 0)
-    j = 0
+    song_transition_probs = {}
+
+    # for every song in the corpus, 'chords' will be a list of the chords
     for chords in chord_lists:
         length = len(chords)
+
+        # for every chord in the list of chords
         for i in range(length-1):
             transition = (chords[i]['root'], chords[i+1]['root'])
-            song_transition = (transition, chords[i]['song_name'])
-            print song_transition
-            song_counts[song_transition] += 1
             transition_counts[transition] += 1
             chord_counts[chords[i]['root']] += 1
-            if i == (length - 1):
-                j += length
-    return chord_counts, transition_counts, song_counts
+
+        song_transition_probs[chords[i]['song_name']] = get_transition_probs(chord_counts, transition_counts)
+
+        # reset the count dictionaries for the next song
+        chord_counts = defaultdict(lambda: 0)
+        transition_counts = defaultdict(lambda: 0)
+
+    return song_transition_probs
 
 def get_transition_probs(chord_counts, transition_counts):
     """
@@ -38,12 +43,15 @@ def get_transition_probs(chord_counts, transition_counts):
     return probs
 
 if __name__ == '__main__':
-    datafile = 'AlldataWithNonHarmonicsV2.csv'
+    datafile = 'AlldataWithNonHarmonicsV5.csv'
     data = read_data(datafile)
-    chord_counts, transition_counts, song_counts = get_overall_counts(data)
-    transition_probs = get_transition_probs(chord_counts, transition_counts)
+    transition_probs = transition_probs_by_song(data)
 
-    # map roman numerals to integers for sorting, and covert back to display
-    transitions = [(RN.index(c1), c2) for c1, c2 in transition_probs]
-    for c1, c2 in sorted(transitions):
-        print '({} -> {}): {:.4f}'.format(RN[c1], c2, transition_probs[(RN[c1], c2)])
+    for song_name, probs in transition_probs.items():
+        print song_name + '\n' + ('-' * len(song_name))
+
+        # map roman numerals to integers for sorting, and covert back to display
+        transitions = [(RN.index(c1), c2) for c1, c2 in probs]
+        for c1, c2 in sorted(transitions):
+            print '({} -> {}): {:.4f}'.format(RN[c1], c2, probs[(RN[c1], c2)])
+        print #newline
