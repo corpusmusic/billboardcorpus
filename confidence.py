e6a528d3a01d8d03d8913a793d2e168003359902
rvlvrocelot <andrew.mahan@colorado.edu>
2014-05-28 22:42:56 -0600

    generates tables for mean transitional probability and confidence intervals

--- confidence.py
+++ confidence.py
@@ -26,32 +26,58 @@ def mean_confidence_interval(data, confidence=0.95):
     return [m, m-h, m+h]
 
 
+def write_csv(table,name):
+	with open(name, 'wb') as f:
+		writer = csv.writer(f)
+		rowNames = [[name,"I","bII","II","bIII","III","IV","bV","V","bVI","VI","bVII","VII"]]
+
+		for index,row in enumerate(table):
+			row.insert(0,rowNames[0][index+1]) 		
+
+		writer.writerows(rowNames)
+		writer.writerows(table)
+
 #generate table
 
+def generate_tables(chunk):
 #assume that the format is I-I, I-bII I-II, I-bIII ... I-VII, II-I, II-bII ... VII-VII 
 
-meanTable = []
-intervalTable = []
-meanTableNoZeros = []
-intervalTableNoZeros = []
+	meanTable = []
+	intervalTable = []
+	meanTableNoZeros = []
+	intervalTableNoZeros = []
 
-disDict = load_csv('outputFIXED2.txt')
-for key in range(145):
-	if key != 0:
-		data = [float(x) for x in disDict[key][1:]]
-		datanozeros = filter(lambda x: x != 0, data)
-		con = mean_confidence_interval(data)
-		conNoZeros = mean_confidence_interval(datanozeros)
+	disDict = load_csv('output4bars.csv')
+	for key in range(145):
+		if key != 0:
+			data = [float(x) for x in disDict[key][1:]]
+			data = data[chunk*730: (chunk+1)*730]	
+	
+			datanozeros = filter(lambda x: x != 0, data)
+			
+			print datanozeros
 
-		meanTable.append(round(con[0],4))
-		meanTableNoZeros.append(round(conNoZeros[0],4))
-		intervalTable.append(str(round(con[1],4)) + "-" + str(round(con[2],4)) )
-		intervalTableNoZeros.append(str(round(conNoZeros[1],4)) + "-" + str(round(conNoZeros[2],4)) )
+			con = mean_confidence_interval(data)
+			conNoZeros = mean_confidence_interval(datanozeros)
+
+			meanTable.append(round(con[0],4))
+			meanTableNoZeros.append(round(conNoZeros[0],4))
+			intervalTable.append(str(round(con[1],4)) + "-" + str(round(con[2],4)) )
+			intervalTableNoZeros.append(str(round(conNoZeros[1],4)) + "-" + str(round(conNoZeros[2],4)) )
 		
-meanTable = [meanTable[i:i+12] for i in xrange(0,len(meanTable),12)]
-meanTableNoZeros = [meanTableNoZeros[i:i+12] for i in xrange(0,len(meanTableNoZeros),12)]
-intervalTable = [intervalTable[i:i+12] for i in xrange(0,len(intervalTable),12)]
-intervalTableNoZeros = [intervalTableNoZeros[i:i+12] for i in xrange(0,len(intervalTableNoZeros),12)]
+	meanTable = [meanTable[i:i+12] for i in xrange(0,len(meanTable),12)]
+	meanTableNoZeros = [meanTableNoZeros[i:i+12] for i in xrange(0,len(meanTableNoZeros),12)]
+	intervalTable = [intervalTable[i:i+12] for i in xrange(0,len(intervalTable),12)]
+	intervalTableNoZeros = [intervalTableNoZeros[i:i+12] for i in xrange(0,len(intervalTableNoZeros),12)]
+
+	write_csv(meanTable,"meanTransistionProbabilities" + str(chunk))
+	write_csv(meanTableNoZeros, "MeanTransitionProbabilitiesNoZero" + str(chunk))
+        write_csv(intervalTable, "ConfidenceIntervalTransitionalProbabilities" + str(chunk))
+        write_csv(intervalTableNoZeros, "ConfidenceIntervalTransitionalProbabilitiesNoZero" + str(chunk))
 	
 		
+generate_tables(0)
+generate_tables(1)
+generate_tables(2)
+generate_tables(3)
 
